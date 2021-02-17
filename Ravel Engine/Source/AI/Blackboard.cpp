47df69a3fc0b775296cd59a7a9176f42e3bfedd8
Sebastian <sebastian.tan@mytona.com>
2021-02-17 15:18:28 +0800

    upd Blackboard getter and setter

--- Ravel Engine/Source/AI/Blackboard.cpp
+++ Ravel Engine/Source/AI/Blackboard.cpp
@@ -1,17 +1,41 @@
 #include "Blackboard.h"
 
-bool Blackboard::GetBlackboardValueAsBool(std::string const& keyID) {
-	return m_Keys[keyID];
+bool Blackboard::GetValueAsBool(std::string_view keyName) {
+	auto* object = dynamic_cast<Object<bool>*>(m_Keys[keyName]);
+	if (object)
+		return object->data();
 }
 
-int Blackboard::GetBlackboardValueAsInt(std::string const& keyID) {
-	return (int)m_Keys[keyID];
+int Blackboard::GetValueAsInt(std::string_view keyName) {
+	auto* object = dynamic_cast<Object<int>*>(m_Keys[keyName]);
+	if (object)
+		return object->data();
 }
 
-float Blackboard::GetBlackboardValueAsFloat(std::string const& keyID) {
-	return dynamic_cast<Object<float>*>(m_Keys[keyID])->data();
+float Blackboard::GetValueAsFloat(std::string_view keyName) {
+	auto* object = dynamic_cast<Object<float>*>(m_Keys[keyName]);
+	if (object)
+		return object->data();
 }
 
-double Blackboard::GetBlackboardValueAsDouble(std::string const& keyID) {
-	return dynamic_cast<Object<double>*>(m_Keys[keyID])->data();
+double Blackboard::GetValueAsDouble(std::string_view keyName) {
+	auto* object = dynamic_cast<Object<double>*>(m_Keys[keyName]);
+	if (object)
+		return object->data();
+}
+
+void Blackboard::SetValueAsBool(std::string_view keyName, bool value) {
+	m_Keys[keyName] = Memory()->alloc<Object<bool>>();
+}
+
+void Blackboard::SetValueAsInt(std::string_view keyName, int value) {
+	m_Keys[keyName] = Memory()->alloc<Object<int>>();
+}
+
+void Blackboard::SetValueAsFloat(std::string_view keyName, float value) {
+	m_Keys[keyName] = Memory()->alloc<Object<float>>();
+}
+
+void Blackboard::SetValueAsDouble(std::string_view keyName, double value) {
+	m_Keys[keyName] = Memory()->alloc<Object<double>>();
 }
--- Ravel Engine/Source/AI/Blackboard.h
+++ Ravel Engine/Source/AI/Blackboard.h
@@ -3,26 +3,56 @@
 
 #include <map>
 #include <string>
+#include <string_view>
+#include "../MemoryManager.h"
 
 class Blackboard {
 public:
 	Blackboard() {};
 	~Blackboard() {};
 
-	// getter and setter
-	bool GetBlackboardValueAsBool(std::string const& keyID);
-	int GetBlackboardValueAsInt(std::string const& keyID);
-	float GetBlackboardValueAsFloat(std::string const& keyID);
-	double GetBlackboardValueAsDouble(std::string const& keyID);
+	// getter
+	bool GetValueAsBool(std::string_view keyName);
+	int GetValueAsInt(std::string_view keyName);
+	float GetValueAsFloat(std::string_view keyName);
+	double GetValueAsDouble(std::string_view keyName);
+	std::string GetValueAsString(std::string_view keyName);
+
+	template<class DataType>
+	DataType GetValue(std::string_view keyName);
 
 	template<class UserObject>
-	UserObject GetBlackboardValueAsObject(std::string const& keyID);
+	UserObject GetValueAsObject(std::string_view keyName);
+
+	// setter
+	void SetValueAsBool(std::string_view keyName, bool value);
+	void SetValueAsInt(std::string_view keyName, int value);
+	void SetValueAsFloat(std::string_view keyName, float value);
+	void SetValueAsDouble(std::string_view keyName, double value);
+
+	template<class DataType>
+	void SetValue(std::string_view keyName, DataType value);
+	
+	template<class UserObject>
+	void SetValueAsObject(std::string_view keyName, UserObject* value);
 
 private:
-	std::map<std::string, BaseObject*> m_Keys;
+	std::map<std::string_view, BaseObject*> m_Keys;
 };
 
 template<class UserObject>
-UserObject Blackboard::GetBlackboardValueAsObject(std::string const& keyID) {
-	return dynamic_cast<Object<UserObject>*>(m_Keys[keyID])->data();
+UserObject Blackboard::GetValueAsObject(std::string_view keyName) {
+	auto* object = dynamic_cast<Object<UserObject>*>(m_Keys[keyID]);
+	if(object)
+		return object->data();
+}
+
+template <class UserObject>
+void Blackboard::SetValueAsObject(std::string_view keyName, UserObject* value) {
+	m_keys[keyName] = value;
+}
+
+template<class DataType>
+void SetValue(std::string_view keyName, DataType value) {
+	m_Keys[keyName] = Memory()->alloc<DataType>();
 }
