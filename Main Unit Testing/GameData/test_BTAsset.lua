b7367d8cac244311b5e809f21a42466ce125393e
Sebastian <sebastian.tan@mytona.com>
2021-02-22 17:32:33 +0800

    upd assets

---
+++ Main Unit Testing/GameData/test_BTAsset.lua
@@ -0,0 +1,13 @@
+test_BTAsset = {}
+
+function test_BTAsset.OnStart(me, them)
+
+end
+
+function test_BTAsset.Update(me, delta)
+	DebugLog("BTAsset Update", delta);
+end
+
+function test_BTAsset.PrintTask(me, data)
+	DebugLog("BTAsset Task print", delta);
+end
